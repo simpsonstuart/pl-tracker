03a448b6f6a080c3f967bcdc3760ce21c1eab330
stuart <stutech@live.com>
2016-04-12 15:17:18 -0600

    -working mongo queries and authentication with google

--- examples/client/controllers/logout.js
+++
@@ -1,9 +0,0 @@
-angular.module('MyApp')
-  .controller('LogoutCtrl', function($location, $auth, toastr, $state) {
-    if (!$auth.isAuthenticated()) { return; }
-    $auth.logout()
-      .then(function() {
-        toastr.info('You have been logged out');
-          $state.go('login')
-      });
-  });
